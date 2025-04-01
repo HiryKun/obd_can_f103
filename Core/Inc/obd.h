84db9aae6ef1a4aeb683809154c75324065877fb
HiryKun <1951086367@qq.com>
2025-04-01 23:59:52 +0800

    第一版OBD驱动库/主程序框架

---
+++ Core/Inc/obd.h
@@ -0,0 +1,144 @@
+/**
+ * @file obd.h
+ * @author HiryKun (1951086367@qq.com)
+ * @brief 适用于CAN的OBD通讯驱动库
+ * @version 0.1
+ * @date 2025-04-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef __OBD_H
+#define __OBD_H
+
+/* 发送/接收缓存大小 */
+#define OBD_BUFFER_SIZE         32
+/* OBD的CAN数据帧长度 */
+#define OBD_CAN_DLC             8
+
+/* OBD请求CAN ID */
+#define OBD_REQ_FUNC_ADDR       0x7DF
+#define OBD_RESP_ECU1_ADDR      0x7E8
+#define OBD_RESP_ECU2_ADDR      0x7E9
+#define OBD_RESP_ECU3_ADDR      0x7EA
+#define OBD_RESP_ECU4_ADDR      0x7EB
+#define OBD_RESP_ECU5_ADDR      0x7EC
+#define OBD_RESP_ECU6_ADDR      0x7ED
+#define OBD_RESP_ECU7_ADDR      0x7EE
+#define OBD_RESP_ECU8_ADDR      0x7EF
+
+/* OBD服务ID，目前只需要01 */
+#define OBD_SERVICE_RX_01       0x41
+#define OBD_SERVICE_TX_01       0x01
+/* OBD PIDs */
+#define OBD_PID_ENGINE_RPM      0x0C
+#define OBD_PID_VEHICLE_SPEED   0x0D
+/* 占位符 */
+#define OBD_TX_PLACEHOLDER      0x00,0x00,0x00,0x00,0x00,0x00
+
+/**
+ * @brief 缓存数据类型，使用结构体存储的循环队列
+ * 
+ */
+typedef struct {
+    uint8_t front;
+    uint8_t rear;
+    uint8_t data[OBD_BUFFER_SIZE][OBD_CAN_DLC];
+} OBD_BufferTypeDef;
+
+/**
+ * @brief 缓存状态枚举类型
+ * 
+ */
+typedef enum {
+    BUFFER_EMPTY    = 0x00,     //缓存为空（空队列）
+    BUFFER_FULL,                //缓存已满（满队列）
+    BUFFER_BUSY                 //缓存有数据（队列不空但未满）
+} OBD_BufferStateTypeDef;
+
+/**
+ * @brief 车辆状态引索
+ * 
+ */
+typedef enum {
+    CAN_STATE      = 0x00,     //CAN状态
+    ENGINE_RPM,                 //引擎转速
+    VEHICLE_SPEED,              //车辆速度
+    VEHICLE_STATUS_SIZE         //枚举末尾，用于记录枚举大小
+} OBD_VehicleStatusIndexTypeDef;
+
+/**
+ * @brief OBD初始化函数
+ * 
+ * 设置CAN滤波器，启动CAN，并激活接收中断
+ * 
+ * @param hcan CAN外设句柄
+ */
+HAL_StatusTypeDef OBD_Init(CAN_HandleTypeDef *hcan);
+
+/**
+ * @brief 查询发送缓存状态
+ * 
+ * @return OBD_BufferStateTypeDef 缓存状态
+ */
+OBD_BufferStateTypeDef OBD_GetTxBufferState(void);
+
+/**
+ * @brief 查询接收缓存状态
+ * 
+ * @return OBD_BufferStateTypeDef 缓存状态
+ */
+OBD_BufferStateTypeDef OBD_GetRxBufferState(void);
+
+/**
+ * @brief OBD发送缓存处理函数
+ * 
+ * 从发送缓存中取出数据，发送到CAN总线上
+ * 
+ * 一次只发送一帧数据，应被频繁调用
+ * 
+ * 但要避免循环长期阻塞，应时常检查 CAN_Error_Code
+ * 
+ * @return HAL_StatusTypeDef 是否成功把数据放入发送邮箱
+ * @retval HAL_OK 成功写入发送邮箱
+ * @retval HAL_BUSY 发送邮箱还有数据未发送，或上次发送后还未接收到回复报文
+ * @retval HAL_ERROR 缓存为空，无需发送
+ */
+HAL_StatusTypeDef OBD_TxBufferProcess(void);
+
+/**
+ * @brief OBD接收缓存处理函数
+ * 
+ * 从接收缓存中取出数据，解析并存储到车辆状态数组中
+ * 
+ * 一次只解析一帧数据，应被频繁调用
+ * 
+ * @return HAL_StatusTypeDef 解析结果
+ * @retval HAL_OK 成功解析
+ * @retval HAL_BUSY 未知PID报文，无法解析
+ * @retval HAL_ERROR 缓存为空，无需解析
+ */
+HAL_StatusTypeDef OBD_RxBufferProcess(void);
+
+/**
+ * @brief 获取车辆状态
+ * 
+ * @param index 车辆状态引索，参见 OBD_VehicleStatusIndexTypeDef
+ * @param status 返回的车辆状态的指针
+ * @return uint32_t CAN错误代码，参见 CAN_Error_Code
+ */
+uint32_t OBD_GetVehicleStatus(OBD_VehicleStatusIndexTypeDef index, uint32_t *pStatus);
+
+
+/**
+ * @brief 发送数据，添加进发送缓存
+ * 
+ * @param data 要发送的数据指针，数据长度应该为8Byte
+ * @return HAL_StatusTypeDef 是否成功添加进发送缓存
+ * @retval HAL_OK 添加成功
+ * @retval HAL_ERROR 缓存已满，添加失败
+ */
+HAL_StatusTypeDef OBD_TxMessage(uint8_t *pData);
+
+#endif
---
+++ Core/Src/obd.c
@@ -0,0 +1,133 @@
+/**
+ * @file obd.c
+ * @author HiryKun (1951086367@qq.com)
+ * @brief 适用于CAN的OBD通讯驱动库
+ * @version 0.1
+ * @date 2025-04-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "stm32f1xx_hal.h"
+#include "obd.h"
+#include <string.h>
+
+uint32_t vehicleStatus[VEHICLE_STATUS_SIZE];
+CAN_HandleTypeDef *hcanOBD;
+uint8_t allowTx = 1;
+static uint32_t lastMailbox = CAN_TX_MAILBOX0;
+static OBD_BufferTypeDef txBuffer, rxBuffer;
+static CAN_TxHeaderTypeDef txHeader = {
+    .StdId  = OBD_REQ_FUNC_ADDR,
+    .ExtId  = 0,
+    .RTR    = CAN_ID_STD,
+    .RTR    = CAN_RTR_DATA,
+    .DLC    = OBD_CAN_DLC,
+   .TransmitGlobalTime = DISABLE
+};
+
+static OBD_BufferStateTypeDef GetBufferState(OBD_BufferTypeDef *pBuffer, uint8_t queueSize) {
+    if (pBuffer->front == pBuffer->rear) return BUFFER_EMPTY;
+    if ((pBuffer->rear + 1) % queueSize == pBuffer->front) return BUFFER_FULL;
+    return BUFFER_BUSY;
+}
+
+static HAL_StatusTypeDef Dequeue(OBD_BufferTypeDef *pBuffer, uint8_t queueSize) {
+    if (GetBufferState(pBuffer, queueSize) == BUFFER_EMPTY) return HAL_ERROR;
+    ++pBuffer->front;
+    if (pBuffer->front >= queueSize) pBuffer->front = 0;
+    return HAL_OK;
+}
+
+static HAL_StatusTypeDef Enqueue(OBD_BufferTypeDef *pBuffer, uint8_t *pData, uint8_t queueSize) {
+    if (GetBufferState(pBuffer, queueSize) == BUFFER_FULL) return HAL_ERROR;
+    memcpy(pBuffer->data[pBuffer->rear], pData, OBD_CAN_DLC);
+    ++pBuffer->rear;
+    if (pBuffer->rear >= queueSize) pBuffer->rear = 0;
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef OBD_Init(CAN_HandleTypeDef *hcan) {
+    hcanOBD = hcan;
+    /* CAN滤波器，只接收ECU1（0x7E8）的报文 */
+    CAN_FilterTypeDef filter = {
+        .FilterIdHigh           = OBD_RESP_ECU1_ADDR << 5,
+        .FilterIdLow            = 0xFFFF,
+        .FilterMaskIdHigh       = 0xFFFF,
+        .FilterMaskIdLow        = 0xFFFF,
+        .FilterFIFOAssignment   = CAN_FILTER_FIFO0,
+        .FilterBank             = 0,
+        .FilterMode             = CAN_FILTERMODE_IDLIST,
+        .FilterScale            = CAN_FILTERSCALE_16BIT,
+        .FilterActivation       = CAN_FILTER_ENABLE,
+        .SlaveStartFilterBank   = 0
+    };
+    HAL_CAN_ConfigFilter(hcanOBD, &filter);
+    HAL_CAN_ActivateNotification(hcanOBD, CAN_IT_RX_FIFO0_MSG_PENDING);
+    HAL_CAN_ActivateNotification(hcanOBD, CAN_IT_ERROR_WARNING);
+    HAL_CAN_ActivateNotification(hcanOBD, CAN_IT_BUSOFF);
+    HAL_CAN_ActivateNotification(hcanOBD, CAN_IT_LAST_ERROR_CODE);
+    HAL_CAN_ActivateNotification(hcanOBD, CAN_IT_ERROR);
+    return HAL_CAN_Start(hcanOBD);
+}
+
+inline OBD_BufferStateTypeDef OBD_GetTxBufferState(void) {
+    return GetBufferState(&txBuffer, OBD_BUFFER_SIZE);
+}
+
+inline OBD_BufferStateTypeDef OBD_GetRxBufferState(void) {
+    return GetBufferState(&rxBuffer, OBD_BUFFER_SIZE);
+}
+
+HAL_StatusTypeDef OBD_TxBufferProcess(void) {
+    if (GetBufferState(&rxBuffer, OBD_BUFFER_SIZE) == BUFFER_EMPTY) return HAL_ERROR;
+    /*
+    * 对于OBD协议，成功发送一个CAN报文后，车辆ECU必有一报文回复
+    * 所以在发送报文后，需要等待接受一次报文，才能继续发送下一个报文
+    * 否则可能导致无意义发送，无法获取车辆信息
+    */
+    if (!allowTx || HAL_CAN_GetTxMailboxesFreeLevel(hcanOBD) < 3) return HAL_BUSY;
+    HAL_CAN_AddTxMessage(hcanOBD, &txHeader, txBuffer.data[txBuffer.front], &lastMailbox);
+    allowTx = 0;
+    Dequeue(&txBuffer, OBD_BUFFER_SIZE);
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef OBD_RxBufferProcess(void) {
+    if (GetBufferState(&rxBuffer, OBD_BUFFER_SIZE) == BUFFER_EMPTY) return HAL_ERROR;
+    uint8_t data[OBD_CAN_DLC];
+    memcpy(data, rxBuffer.data[rxBuffer.front], OBD_CAN_DLC);
+    Dequeue(&rxBuffer, OBD_BUFFER_SIZE);
+    if (data[1] != OBD_SERVICE_RX_01) return HAL_BUSY;
+    switch (data[2]) {
+    case OBD_PID_ENGINE_RPM:
+        vehicleStatus[ENGINE_RPM] = (data[3] << 8) | data[4];
+        break;
+    case OBD_PID_VEHICLE_SPEED:
+        vehicleStatus[VEHICLE_SPEED] = data[3];
+    default:
+        return HAL_BUSY;
+        break;
+    }
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef OBD_TxMessage(uint8_t *pData) {
+    return Enqueue(&txBuffer, pData, OBD_BUFFER_SIZE);
+}
+
+uint32_t OBD_GetVehicleStatus(OBD_VehicleStatusIndexTypeDef index, uint32_t *pStatus) {
+    if (index < VEHICLE_STATUS_SIZE) *pStatus = vehicleStatus[index];
+    return vehicleStatus[CAN_STATE];
+}
+
+/*接收中断处理函数*/
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+    if (GetBufferState(&rxBuffer, OBD_BUFFER_SIZE) == BUFFER_EMPTY) return;  //接收缓存已满，不读取邮箱，根据CAN控制器配置溢出
+    allowTx = 1;
+    CAN_RxHeaderTypeDef rx_header;
+    uint8_t data[OBD_CAN_DLC];
+    HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, data);
+    Enqueue(&rxBuffer, data, OBD_BUFFER_SIZE);
+}
